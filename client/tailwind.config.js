70f7aa2ef73c9a32b68b62522a09a77921cd236d
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2025-10-30 03:53:04 +0530

    Added flowbite tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,28 @@
+// /** @type {import('tailwindcss').Config} */
+// import flowbite from "flowbite-react/tailwind";
+
+// export default {
+//   content:[
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind";
+
 export default {
-  content:[
+  content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    "./node_modules/flowbite/**/*.js",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  theme: { extend: {} },
+  plugins: [flowbite],
 }
 
